9c79b1da76e3e0320f97c95a6fb27eba93fa5395
Daniele <dmafrici84@gmail.com>
2020-07-07 17:25:11 +0200

    Lc js-html-css-revolut finito

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+    // Quando l'utente scorre la pagina, esegue la funzione scroola
+  window.onscroll = function() {scrolla()};
+
   // al passaggio del cursore sul link della nav si apre il menu corrispondente
   $(".header-nav-bottom-central .parent-dropdown ").mouseenter(function(){
     $(this).children(".dropdown").addClass("visibile");
@@ -14,6 +17,7 @@ $(document).ready(function(){
   $(".header-nav-bottom-right .hamburger").click(
     function() {
       $(".hamburger-menu").addClass("visibile");
+      $("body").addClass("noScrollbar");
     }
   );
 
@@ -21,23 +25,51 @@ $(document).ready(function(){
   $(".hamburger-menu .chiudi").click(
     function() {
       $(".hamburger-menu").removeClass("visibile");
+      $("body").removeClass("noScrollbar");
     }
   );
 
+  // al click sui tag li del menu hamburger
   $(".hamburger-menu-main .parent-dropdown").click(
     function() {
-      console.log($(".hamburger-dropdown").hasClass("visibile"));
 
-      if($(this).children(".hamburger-dropdown").hasClass("visibile")) {
+      var hamburgerDropdown = $(this).children(".hamburger-dropdown");
 
-        $(this).children(".hamburger-dropdown").removeClass("visibile");
-        $(this).children(".hamburger-menu-linck").children("a").removeClass("color");
+      var cambiaColore = $(this).children(".hamburger-menu-linck").children("a");
+
+      if(hamburgerDropdown.hasClass("visibile")) {
+
+        hamburgerDropdown.removeClass("visibile");
+        cambiaColore.removeClass("color");
+
       } else {
-        $(this).children(".hamburger-dropdown").addClass("visibile");
-        $(this).children(".hamburger-menu-linck").children("a").addClass("color");
+
+        hamburgerDropdown.addClass("visibile");
+        cambiaColore.addClass("color");
+
       }
 
-    }
-  );
+    });
+
+
 
 });
+
+// funzioni
+
+  // funzione scrolla
+
+    // Ottieni la barra di navigazione
+    var navbar = document.getElementById("header-nav-bottom");
+
+    // Ottieni la posizione di offset della barra di navigazione
+    var sticky = navbar.offsetTop;
+
+    // Aggiungi la classe sticky alla barra di navigazione quando raggiungi la sua posizione di scorrimento. Rimuovere sticky quando si lascia la posizione di scorrimento
+    function scrolla() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
